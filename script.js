cbad9e84e9655fa63d972a33a460fe9e957a3635
Joal22 <GitYGitHubgmail.com>
2023-10-23 21:10:19 -0600

    subiendo Proyecto

--- script.js
+++ script.js
@@ -1,94 +1,118 @@
 var opciones = [];
+var datosJSON; // Declaración global para almacenar los datos JSON
 
-        document.addEventListener("DOMContentLoaded", function() {
-            fetch('ejercicios-basicos.json')
-                .then(response => response.json())
-                .then(data => {
-                    // Datos JSON cargados con éxito
-                    var datosJSON = data;
+document.addEventListener("DOMContentLoaded", function () {
+  fetch('ejercicios-basicos.json')
+    .then(response => response.json())
+    .then(data => {
+      datosJSON = data; // Almacenar los datos JSON
 
-                    // Obtén una referencia a los elementos en HTML
-                    var preguntaContainer = document.getElementById("pregunta");
-                    var opcionesRespuesta = document.getElementById("opcionesRespuesta");
+      var preguntaContainer = document.getElementById("pregunta");
+      var opcionesRespuesta = document.getElementById("opcionesRespuesta");
+      var cuadroRespuestas = document.getElementById("cuadroRespuestas");
 
-                    // Variables para el seguimiento de preguntas y arreglos
-                    var currentArrayIndex = 0;
-                    var currentQuestionIndex = 0;
+      var currentArrayIndex = 0;
+      var currentQuestionIndex = 0;
+      function mostrarPregunta() {
+        if (currentArrayIndex < datosJSON.length) {
+          var pregunta = datosJSON[currentArrayIndex][currentQuestionIndex];
+          preguntaContainer.querySelector("h2").textContent = pregunta.categoria;
+          
+          var imagenPregunta = preguntaContainer.querySelector(".imgPregunta");
+          imagenPregunta.src = pregunta.pregunta;
+          imagenPregunta.alt = "Pregunta";
 
-                    // Función para mostrar una pregunta
-                    function mostrarPregunta() {
-                        var pregunta = datosJSON[currentArrayIndex][currentQuestionIndex];
-                        preguntaContainer.querySelector("h2").textContent = pregunta.categoria;
-                    
-                        var imagenPregunta = preguntaContainer.querySelector("img");
-                        imagenPregunta.src = pregunta.pregunta;
-                        imagenPregunta.alt = "Pregunta";
-                    
-                        // Almacena las opciones de respuesta en la variable global
-                        opciones = [pregunta.respuesta, pregunta.incorrecta1, pregunta.incorrecta2];
-                        opciones = shuffle(opciones);
-                    
-                        opcionesRespuesta.innerHTML = '';
-                        opciones.forEach(function(opcion) {
-                            var botonRespuesta = document.createElement("button");
-                            var imagenOpcion = document.createElement("img");
-                            imagenOpcion.src = opcion;
-                            imagenOpcion.alt = "Opción de respuesta";
-                            botonRespuesta.appendChild(imagenOpcion);
-                            botonRespuesta.addEventListener("click", function() {
-                                verificarRespuesta(opcion);
-                            });
-                            opcionesRespuesta.appendChild(botonRespuesta);
-                        });
-                    }
-                    
+          opciones = [pregunta.respuesta, pregunta.incorrecta1, pregunta.incorrecta2];
+          opciones = shuffle(opciones);
 
-                    // Función para verificar la respuesta
-                    function verificarRespuesta(respuestaUsuario) {
-                        var pregunta = datosJSON[currentArrayIndex][currentQuestionIndex];
-                        var cuadroRespuestas = document.getElementById("cuadroRespuestas");
-                    
-                        if (respuestaUsuario === pregunta.respuesta) {
-                           
-                            
-                            if (currentQuestionIndex === datosJSON[currentArrayIndex].length - 1) {
-                                cuadroRespuestas.innerHTML = ''; // Borra el cuadro de respuestas al finalizar el conjunto
-                            }
-                    
-                            // Agrega el ejercicio al cuadro de respuestas correctas
-                            var imagenEjercicio = document.createElement("img");
-                            imagenEjercicio.src = pregunta.pregunta;
-                            cuadroRespuestas.appendChild(imagenEjercicio);
-                            
-                            currentQuestionIndex++;
-                            
-                            if (currentQuestionIndex >= datosJSON[currentArrayIndex].length) {
-                                currentArrayIndex++;
-                                currentQuestionIndex = 0;
-                            }
-                            
-                            if (currentArrayIndex < datosJSON.length) {
-                                mostrarPregunta();
-                            } else {
-                                alert("Has completado todas las preguntas.");
-                            }
-                        } else {
-                            alert("Respuesta incorrecta. Inténtalo de nuevo.");
-                        }
-                    }
-                    
+          opcionesRespuesta.innerHTML = '';
+          opciones.forEach(function (opcion) {
+            var botonRespuesta = document.createElement("button");
+            var imagenOpcion = document.createElement("img");
+            imagenOpcion.src = opcion;
+            imagenOpcion.alt = "Opción de respuesta";
+            // Modifica el tamaño de las imágenes
+            imagenOpcion.style.width = "150px";
+            imagenOpcion.style.height = "75px";
+            botonRespuesta.appendChild(imagenOpcion);
+            botonRespuesta.addEventListener("click", function () {
+              verificarRespuesta(opcion);
+            });
+            opcionesRespuesta.appendChild(botonRespuesta);
+          });
+          // Agrega el ejercicio inicial al cuadro de respuestas al cargar un nuevo conjunto
+          if (currentQuestionIndex === 0) {
+            var imagenEjercicioInicial = document.createElement("img");
+            imagenEjercicioInicial.src = pregunta.pregunta; // Usamos la imagen de la pregunta como ejercicio inicial
+            // Modifica el tamaño de la imagen
+            imagenEjercicioInicial.style.width = "150px";
+            imagenEjercicioInicial.style.height = "75px";
+            cuadroRespuestas.appendChild(imagenEjercicioInicial);
+          }
+          
+        } else {
+          alert("Has completado todas las preguntas.");
+        }
+      }
 
-                    // Función para barajar las opciones de respuesta
-                    function shuffle(array) {
-                        for (var i = array.length - 1; i > 0; i--) {
-                            var j = Math.floor(Math.random() * (i + 1));
-                            [array[i], array[j]] = [array[j], array[i]];
-                        }
-                        return array;
-                    }
+      function verificarRespuesta(respuestaUsuario) {
+        if (currentArrayIndex < datosJSON.length) {
+          var pregunta = datosJSON[currentArrayIndex][currentQuestionIndex];
+      
+          if (respuestaUsuario === pregunta.respuesta) {
+            // Agrega el ejercicio al cuadro de respuestas correctas
+            var imagenEjercicio = document.createElement("img");
+            imagenEjercicio.src = pregunta.respuesta;
+            imagenEjercicio.style.width="150px";
+            imagenEjercicio.style.height="75px";
+            cuadroRespuestas.appendChild(imagenEjercicio);
+            alert("Excelente");
+      
+            if (currentQuestionIndex === datosJSON[currentArrayIndex].length - 1) {
+              cuadroRespuestas.innerHTML = ''; // Limpia el cuadro de respuestas antes de avanzar
+              currentQuestionIndex++;
+              if (currentQuestionIndex>0) {
+                alert("cambiando")
+              }
+      
+              if (currentQuestionIndex >= datosJSON[currentArrayIndex].length) {
+                currentArrayIndex++;
+                currentQuestionIndex = 0;
+              }
+      
+              mostrarPregunta(); // Muestra la siguiente pregunta después de mostrar "Cambiando"
+            } else {
+              currentQuestionIndex++;
+              opcionesRespuesta.innerHTML = ''; // Limpia las opciones de respuesta
+              mostrarPregunta(); // Muestra la siguiente pregunta
+            }
+          } else {
+            cuadroRespuestas.innerHTML = ''; // Limpiar el cuadro de respuestas
+            alert("Respuesta incorrecta. Regresando al paso 1 del arreglo.");
+            currentQuestionIndex = 0;
+            mostrarPregunta(); // Vuelve al paso 1 del arreglo en caso de respuesta incorrecta
+          }
+        } else {
+          alert("Has completado todas las preguntas.");
+        }
+      }
+      
+      
+      
 
-                    // Inicializar mostrando la primera pregunta
-                    mostrarPregunta();
-                })
-                .catch(error => console.error('Error al cargar los datos JSON:', error));
-        });
+      
+      
+      
+
+      function shuffle(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+      }
+
+      mostrarPregunta();
+    })
+    .catch(error => console.error('Error al cargar los datos JSON:', error));
+});
